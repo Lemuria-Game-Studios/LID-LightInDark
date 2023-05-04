9fd89a5f39d960b73bb05ccf460b318401214e71
LAPTOP-1V1GMI47\Kaan <kaan.cuhadar@outlook.com>
2023-05-04 22:55:47 +0300

    Merge Kaan-Dev into main

--- Assets/Scripts/Enemy/EnemyMover.cs
+++ Assets/Scripts/Enemy/EnemyMover.cs
@@ -6,15 +6,15 @@ namespace Enemy
 {
     public class EnemyMover : MonoBehaviour
     {
-        public Vector3 WanderCenterPosition;
-        public float WanderRange;
+        public Vector3 wanderCenterPosition;
+        public float wanderRange;
+        public bool isMovingToPlayer;
 
         private bool _isDestinationSet;
 
         private NavMeshAgent _nav;
 
-        [HideInInspector] public bool isMovingToPlayer;
-        [HideInInspector] public bool canMove = true;
+        public bool canMove = true;
         [HideInInspector] public Transform target;
 
         private void Awake()
@@ -31,8 +31,6 @@ namespace Enemy
 
         private void SetDestination()
         {
-            if (!canMove) return;
-            
             if (isMovingToPlayer)
             {
                 _isDestinationSet = true;
@@ -41,7 +39,7 @@ namespace Enemy
             else if (!_isDestinationSet)
             {
                 _isDestinationSet = true;
-                var randomPos = Random.insideUnitSphere * WanderRange + WanderCenterPosition;
+                var randomPos = Random.insideUnitSphere * wanderRange + wanderCenterPosition;
                 _nav.destination = new Vector3(randomPos.x, transform.position.y, randomPos.z);
                 if (NavMesh.SamplePosition(_nav.destination, out NavMeshHit hit, 20, NavMesh.GetAreaFromName("Walkable")))
                 {
@@ -53,12 +51,11 @@ namespace Enemy
         private void SetIfCanMove()
         {
             _nav.isStopped = !canMove;
-            if (!canMove) GetComponent<NavMeshAgent>().velocity = Vector3.zero;
         }
         
         private void CheckIfReached()
         {
-            if (Vector3.Distance(_nav.destination, transform.position) < 1f || _nav.isStopped)
+            if (Vector3.Distance(_nav.destination, transform.position) < 1f)
             {
                 StartCoroutine(SetIsDestinationSetFalseAfterDelay());
             }
