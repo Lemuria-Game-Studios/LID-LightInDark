69f1f932574eaa183088e92a747d8e942ee1fccb
LAPTOP-1V1GMI47\Kaan <kaan.cuhadar@outlook.com>
2023-06-07 23:12:51 +0300

    Roof Opacity method change

--- Assets/Scripts/GameObjects/ObjectOpacity.cs
+++ Assets/Scripts/GameObjects/ObjectOpacity.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace GameObjects
@@ -7,7 +9,8 @@ namespace GameObjects
     {
         private Renderer _renderer;
         [SerializeField] private float reduceSpeed;
-        private float _target;
+        [SerializeField] private float _target = 255;
+        private Material[] materials;
 
         private void Awake()
         {
@@ -15,18 +18,23 @@ namespace GameObjects
         }
 
         //[SerializeField] private LayerMask mask;
-       
+        private void OnEnable()
+        {
+            _target = 255;
+            _renderer = GetComponent<Renderer>();
+            materials = _renderer.materials;
+        }
+
         /*private void Update()
         {
-            if (_target != null)
-            {
-                float newAlpha = Mathf.MoveTowards(_renderer.material.color.a, _target, reduceSpeed * Time.deltaTime);
-                var material = _renderer.material;
-                var materialColor = _renderer.material.color;
-                materialColor.a = newAlpha;
-                material.color = materialColor;
-            }
-                
+            Color32 materialColor = _renderer.material.color;
+                var moveTowards = Mathf.MoveTowards(materialColor.a, _target, reduceSpeed * Time.deltaTime);
+                materialColor.a = (byte)moveTowards;
+                //_renderer.material.color = materialColor;
+                foreach (Material material in materials)
+                {
+                    material.color = materialColor;
+                }
         }*/
         
 
@@ -35,10 +43,11 @@ namespace GameObjects
             if (other.gameObject.layer ==  LayerMask.NameToLayer("Player"))
             {
                 Debug.Log("Girdi");
-                Color32 materialColor = _renderer.material.color;
-                //_target = 128;
-                materialColor.a = 128; 
-                _renderer.material.color = materialColor;
+                //Color32 materialColor = _renderer.material.color;
+                _target = 0;
+                StartCoroutine(UpdateMaterialColor());
+                // materialColor.a = 0; 
+                //_renderer.material.color = materialColor;
             }
         }
 
@@ -46,11 +55,30 @@ namespace GameObjects
         {
             if (other.gameObject.layer ==  LayerMask.NameToLayer("Player"))
             {
+                Debug.Log("Cıktı");
                 var renderer = gameObject.GetComponent<Renderer>();
                 Color32 materialColor = renderer.material.color;
+                _target = 255;
                 materialColor.a = 255; // 0.5f * 255 (alpha değerini 0-255 aralığında belirtmek için)
                 renderer.material.color = materialColor;
                 
+            }
+        }
+        private IEnumerator UpdateMaterialColor()
+        {
+            while (Mathf.Abs(_renderer.material.color.a - _target) > 0.01f)
+            {
+                Color32 materialColor = _renderer.material.color;
+                float newAlpha = Mathf.MoveTowards(materialColor.a, _target, reduceSpeed * Time.deltaTime);
+                materialColor.a = (byte)newAlpha;
+        
+                foreach (Material material in materials)
+                {
+                    material.color = materialColor;
+                }
+
+                _renderer.material.color = materialColor;
+                yield return null;
             }
         }
     }
